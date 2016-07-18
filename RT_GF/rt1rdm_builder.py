bb55720c6a7fc9320c64bfeac5e5c63043315897
Enrico Ronca <eronca@caltech.edu>
2016-07-17 18:32:30 -0700

    RT1RDM_BUILDER has been changed to work with new fast version of the TD-1RDM calculation in Block  On branch master

--- RT_GF/rt1rdm_builder.py
+++ RT_GF/rt1rdm_builder.py
@@ -4,12 +4,27 @@ import os, sys, math
 import numpy as np
 
 def input_name(nt):
-    inputname = 'green.'+str(nt)+'.'+str(nt)+'.txt'
+    inputname = 'onepdm.'+str(nt)+'.'+str(nt)+'.txt'
     return inputname
 
 def output_name(nt,component,delta_t):
     outputname = component+'_1rdm_'+str(nt*delta_t)+'.txt'
     return outputname
+
+def read_input(input_file):
+    in_file = open(input_file,"r")
+    norb = int(in_file.readline())
+    onepdm = np.zeros((norb,norb)) 
+  
+    for line in range(norb*norb): 
+        rdm_line = in_file.readline().split()
+        i = int(rdm_line[0])
+        j = int(rdm_line[1])
+        rdm_value = float(rdm_line[2])
+        onepdm[i][j] = rdm_value
+
+    in_file.close()    
+    return onepdm
 
 def run(tot_t, delta_t, scratch):
 
@@ -21,7 +36,7 @@ def run(tot_t, delta_t, scratch):
 
   #Calculate and save 1RDM at t = 0
   nt = 0
-  Re_1rdm = np.loadtxt(scratch+input_name(nt))
+  Re_1rdm = read_input(scratch+input_name(nt))
   Im_1rdm = np.zeros((Re_1rdm.shape))
 
   np.savetxt(output_name(nt,'Re',delta_t),Re_1rdm,fmt='%10.5f')
@@ -29,12 +44,12 @@ def run(tot_t, delta_t, scratch):
 
   #Calculate and save 1RDM at the following times
   for nt in range(1,nsteps+1):
-    ReRe_1rdm = np.loadtxt(scratch+input_name(nt))
-    ImRe_1rdm = np.loadtxt(scratch+input_name(nt+100000))
-    ImIm_1rdm = np.loadtxt(scratch+input_name(nt+200000))
+    ReRe_1rdm = read_input(scratch+input_name(nt))
+    ImRe_1rdm = read_input(scratch+input_name(nt+100000))
+    ImIm_1rdm = read_input(scratch+input_name(nt+200000))
 
     Re_1rdm = ReRe_1rdm - ImIm_1rdm
-    Im_1rdm = -2*ImRe_1rdm
+    Im_1rdm = 2.0*ImRe_1rdm
 
     np.savetxt(output_name(nt,'Re',delta_t),Re_1rdm,fmt='%10.5f')
     np.savetxt(output_name(nt,'Im',delta_t),Im_1rdm,fmt='%10.5f')

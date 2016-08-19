64c01a8e9ec92f851da74eb0c7e85aed5699ab99
Enrico Ronca <eronca@caltech.edu>
2016-08-19 14:27:41 -0700

    The hubbard_1d.py integral generator has been updated to take care also of   periodic boundary conditions.  On branch master

--- Integrals_dump/hubbard_1d.py
+++ Integrals_dump/hubbard_1d.py
@@ -12,14 +12,18 @@ def write_coupling(U,f,nsites):
     for i in range(1,nsites+1):
       print >>f, '{0:.12e}'.format(U), i,i,i,i
 
-def write_hopping(t,f,nsites):
+def write_hopping(t,f,nsites,pbc):
     print >>f, '{0:.12e}'.format(t), 1,2,0,0
+    if (pbc):
+       print >>f, '{0:.12e}'.format(t), 1,nsites,0,0
     for i in range(2,nsites):
       print >>f, '{0:.12e}'.format(t), i,i-1,0,0
       print >>f, '{0:.12e}'.format(t), i,i+1,0,0
+    if (pbc):
+       print >>f, '{0:.12e}'.format(t), nsites,1,0,0
     print >>f, '{0:.12e}'.format(t), nsites,nsites-1,0,0
 
-def run(nsites, t, U, fname):
+def run(nsites, t, U, fname, pbc):
 
     nelec = nsites
 
@@ -33,11 +37,11 @@ def run(nsites, t, U, fname):
     #dumping coupling integrals
     write_coupling(U,f,nsites)
     #dumping of hopping integrals
-    write_hopping(t,f,nsites)
+    write_hopping(t,f,nsites, pbc)
     print >> f, '{0:.12e}'.format(0),0,0,0,0
     f.close()
 
 
 if __name__=="__main__":
     import sys
-    run(int(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), sys.argv[4])
+    run(int(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), sys.argv[4], eval(sys.argv[5]))
